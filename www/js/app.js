98f57e76813f5a03d2afb074b8e0080751cb5e7e
masary2989 <fxxkinspam@gmail.com>
2017-04-06 03:24:42 +0900

    js/app.js追記 ログイン画面に遷移できない

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,26 @@
-var myapp = ons.bootstrap(['onsen']);
+var myapp = ons.bootstrap(['onsen','googleApi']);
+
+// myapp.config(function(googleLoginProvider) {
+//         googleLoginProvider.configure({
+//             clientId: '25604735847-3hrfjp74pv66pqhvc7u6nho2iok2hts7.apps.googleusercontent.com',
+//             scopes: ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/plus.login"]
+//         });
+//     });
+
+ons.enableAutoStatusBarFill();
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDCJYLqPoA6Irb_FIg5Bh28QAM58G_Qlrk",
+    authDomain: "kintaiapp-2006e.firebaseapp.com",
+    databaseURL: "https://kintaiapp-2006e.firebaseio.com",
+    projectId: "kintaiapp-2006e",
+    storageBucket: "kintaiapp-2006e.appspot.com",
+    messagingSenderId: "656106544976"
+  };
+
+console.log(config);
+
+firebase.initializeApp(config);
+
+console.log("app.js loaded!");
