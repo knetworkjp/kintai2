413f7a14e8133d8a8838553689b53a79503c7c26
masary2989 <fxxkinspam@gmail.com>
2017-04-05 21:42:47 +0900

    コントローラ追加成功（全角スペースには注意)

--- www/js/controller/prof_menu.js
+++ www/js/controller/prof_menu.js
@@ -1,7 +1,7 @@
-myapp.controller('ProfMenuController', function($scope) {
-  /*$scope.dialogs = {};
+myapp.controller('ProfMenuController', ['$scope',function($scope) {
+  $scope.dialogs = {};
 
-  this.show = function(dlg) {
+  $scope.show = function(dlg) {
       console.log("run_show_method");
     if (!$scope.dialogs[dlg]) {
       ons.createDialog(dlg).then(function(dialog) {
@@ -13,12 +13,12 @@ myapp.controller('ProfMenuController', function($scope) {
     }
   };
 
-*/
+
     //console.log("Prof_MenuController is ready!");
     ons.ready(function() {
         console.log("Prof_MenuController is ready!");
     });
     
-});
+}]);
   
 //console.log("Prof_MenuController is ready!");
