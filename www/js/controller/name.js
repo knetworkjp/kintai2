e14d8d7cd9887b298741d492394903065b0197a0
masary2989 <fxxkinspam@gmail.com>
2017-04-07 19:40:38 +0900

    実機でAPI確認＝＞通信失敗

--- www/js/controller/name.js
+++ www/js/controller/name.js
@@ -1,4 +1,5 @@
 myapp.controller('NameController', function($scope, $http) {
+    $scope.resultajax
     
     ons.ready(function() {
         console.log("NameController is ready!");
@@ -15,10 +16,12 @@ myapp.controller('NameController', function($scope, $http) {
           success(function(status, headers, config) {
              console.log(date); 
              console.log("ajax successed"); 
+             $scope.resultajax="success";
           }).
           error(function(data, status, headers, config) {
              console.log(status);
              console.log("ajax failed");
+             $scope.resultajax="failed";
           });
     });
     
